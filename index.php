7730bfd78831f4181468de4c23165779185834a1
boryskrol <113014021+boryskrol@users.noreply.github.com>
2024-09-23 16:45:48 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Skrypt PHP</title>
+</head>
+<body>
+    <p>
+    <?php
+       $liczbacalkowita = 5;
+       $liczbarzeczywista = 1.15;
+       $ciagznakow = "witam";
+
+       echo "Liczba calkowita: $liczbacalkowita <br>";
+       echo "Liczba rzeczywsita:  $liczbarzeczywista  <br>";
+       echo "ciag znakow: $ciagznakow  <br>";
+    ?>
+    </p>
+</body>
+</html>
